652408b341155d5f6478dd53effc85bd556586c3
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-05-25 18:04:07 +0200

    Fix bug with hours of only 1 char (8:15)

--- test/hs/timesheet_test.clj
+++ test/hs/timesheet_test.clj
@@ -8,7 +8,8 @@
   (are [in out] (= out (sut/parse-entry-title in))
     "11:30 - 12:30 Foobar" [1.0 "Foobar"]
     "11 - 12 Foobar" [1.0 "Foobar"]
-    "16:45 - 18 Foobar" [1.25 "Foobar"]))
+    "16:45 - 18 Foobar" [1.25 "Foobar"]
+    "8:45 - 9 Mails" [0.25 "Mails"]))
 
 (deftest parse-weekday
   (are [a b m d] (= (t/date-time (t/year (t/now)) m d) (sut/parse-weekday a b))
